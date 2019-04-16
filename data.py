16dadbbd3d751b0c30e7409e7087cd445f90daf9
刘钊 <liuzhao@liuzhaodeMBP.lan>
2019-04-16 09:39:23 +0800

    add new function

--- data.py
+++ data.py
@@ -7,10 +7,22 @@ with open('reviews.txt','r') as f:
         count += 1
         if count % 100000 == 0:
             print(len(data))
-print(len(data))
+print('档案读取完成，总共有',len(data),'笔资料')
 
-print(data[0])
+sum_len = 0
+for d in data:
+	sum_len = sum_len +len(d)
+print('平均长度是',sum_len/len(data))
 
-print('-------------------------------')
 
-print(data[1])
+new = []
+
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new),'笔留言长度小于100')
+print(new[0])
+print(new[1])
+
+
+
